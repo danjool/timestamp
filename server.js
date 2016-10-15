28436f51458955a4af29f9c79a794928cb8d77fe
danjool <danny.craig.martin+c9@gmail.com>
2016-10-15 22:09:25 +0000

    setup route for timestamps and dates

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res)=>{
-   res.send('hello world'); 
+app.get('/favicon.ico', function(req, res) {
+    res.sendStatus(200);
+});
+
+app.get('/:input', (req, res)=>{
+    var input = req.params.input;
+    var times = {unix:null,
+    natural:null}
+    
+    var parsed = Date.parse( input );
+    console.log( "input " + input);
+    if ( !isNaN(parsed) )
+    {
+      times.natural = input;
+      times.unix = parsed;
+    } else {
+        var date = new Date( Number(input) );
+        times.natural = date.toDateString();
+        times.unix = input
+    } 
+    
+    res.send( times ); 
 });
 
 app.listen(process.env.PORT, function(){
